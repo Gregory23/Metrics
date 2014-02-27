3c711f11ec6d186ba4be1b5337800f99b00587e1
Gregory23 <sendra.grzegorz@gmail.com>
2014-02-27 18:31:59 +0100

    First

--- metrics/src/main/java/metrics/GitTest.java
+++ metrics/src/main/java/metrics/GitTest.java
@@ -3,8 +3,7 @@ package metrics;
 public class GitTest {
 	 public static void main(String[] args) {
 		    System.out.println("Git is fun");
-		
 		        System.out.println("Git is fun");
-		      
+		     
 		  }
 }
